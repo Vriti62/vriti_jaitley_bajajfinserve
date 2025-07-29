58c49189942d4f5a5702dcfbdec3c34ff4ecb793
vriti62 <jaitleyvriti@gmail.com>
2025-07-29 21:11:46 +0530

    fixed

--- controllers/bfhl.controller.js
+++ controllers/bfhl.controller.js
@@ -2,41 +2,53 @@ exports.handleBfhlPost = (req, res) => {
   try {
     const data = req.body.data;
 
-    //a check to validate if data incoming is in array format 
+     //a check to validate if data incoming is in array format 
     if (!Array.isArray(data)) {
       return res.status(400).json({ success: false, message: "Invalid input format." });
     }
 
-    //parameters for user_id 
+     //parameters for user_id 
     const fullName = "vriti_jaitley";
     const dob = "31012004";
     const email = "vriti.jaitley@chitkara.edu.in";
-    const roll_number = "2210994862"; 
+    const roll_number = "2210994862";
 
-    const numbers = [];
+    const odd_numbers = [];
+    const even_numbers = [];
     const alphabets = [];
-    const specialChars = [];
+    const special_characters = [];
 
-    //checking for numbers, alphabets and specialChara according to instructions in the sheet
+    let sum = 0;
+    let concat_string = "";
+
     data.forEach(item => {
       if (/^[0-9]+$/.test(item)) {
-        numbers.push(item);
+        const num = parseInt(item);
+        sum += num;
+        if (num % 2 === 0) {
+          even_numbers.push(item);
+        } else {
+          odd_numbers.push(item);
+        }
       } else if (/^[a-zA-Z]$/.test(item)) {
         alphabets.push(item);
+        concat_string += item;
       } else {
-        specialChars.push(item);
+        special_characters.push(item);
       }
     });
 
-    //creating the response 
     const response = {
       is_success: true,
-      user_id:`${fullName.toLowerCase()}_${dob}`,
+      user_id: `${fullName.toLowerCase()}_${dob}`,
       email,
-      roll_number: roll_number,
-      numbers,
+      roll_number,
+      odd_numbers,
+      even_numbers,
       alphabets,
-      special_characters: specialChars
+      special_characters,
+      sum: sum.toString(), 
+      concat_string,
     };
 
     res.status(200).json(response);
@@ -45,4 +57,3 @@ exports.handleBfhlPost = (req, res) => {
     res.status(500).json({ success: false, message: "Something went wrong." });
   }
 };
-
